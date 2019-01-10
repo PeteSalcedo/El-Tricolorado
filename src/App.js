2b1d7ea9c507a90474e05e6291d44e8481450e21
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2019-01-09 20:50:07 -0600

    Uploading header component with the app bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from './Components/Header_Footer/Header.js'
 
 class App extends Component {
     render(){
         return (
             <div className="App">
-            hellos
+            <Header />
             </div>
         );
     }
--- src/Components/Header_Footer/Header.js
+++ src/Components/Header_Footer/Header.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+
 
 class Header extends Component {
     render() {
         return (
-            <h1>Test</h1>      
+            <AppBar
+            position="fixed"
+            style={{
+                backgroundColor:'#98c5e9',
+                boxShadow: 'none',
+                padding:'10px 0',
+                borderBottom: '2px solid #00285e'
+            }}></AppBar>
+            
      
         )
     }
