dc3f23d8a4e26bc87a71e21b37fd3bb5db361283
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2019-01-11 20:39:53 -0600

    Footer rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './Components/Header_Footer/Header.js'
+import Footer from './Components/Header_Footer/Footer.js'
 
 class App extends Component {
     render(){
         return (
             <div className="App">
             <Header />
+            <Footer />
             </div>
         );
     }
