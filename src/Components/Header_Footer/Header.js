28d19afefee9ff5ec12c875db2619b30e7e2df0e
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2019-01-12 23:39:51 -0600

    adding tool bar

--- src/Components/Header_Footer/Header.js
+++ src/Components/Header_Footer/Header.js
@@ -13,6 +13,17 @@ class Header extends Component {
                 padding:'10px 0',
                 borderBottom: '2px solid #00285e'
             }}></AppBar>
+                    <Toolbar style={{display:'flex'}}>
+                    <div style={{flexGrow: 1}}>
+                        <div className="header_logo">
+                            <CityLogo
+                                link={true}
+                                linkTo="/"
+                                width="70px"
+                                height="70px"
+                            />
+                        </div>  
+                    </div>
             
      
         )
