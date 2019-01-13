9b1125b9c34edb7690b3245d7fc7361ae991d3af
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2019-01-13 00:11:56 -0600

    Adding folder to add the react

---
+++ src/Components/admin/matches/index.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import AdminLayout from '../../../Hoc/AdminLayout';
+
+
+
+class AdminMatches extends Component {
+
+
+
+}
+
+export default AdminMatches;
