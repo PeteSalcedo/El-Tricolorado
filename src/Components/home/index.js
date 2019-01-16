bea55cfa02811b095d6f711308762c456cc6f67c
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2019-01-15 21:58:48 -0600

    Adding the home main component

---
+++ src/Components/home/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Featured from './featured';
+//import Matches from './matches';
+//import MeetPlayers from './meetPlayers';
+//import Promotion from './promotion';
