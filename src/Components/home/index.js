eca5f169d6f564fa6b01b292cc7e02632dd29066
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2019-01-15 21:59:55 -0600

    Adding the home main component

--- src/Components/home/index.js
+++ src/Components/home/index.js
@@ -3,3 +3,10 @@ import Featured from './featured';
 //import Matches from './matches';
 //import MeetPlayers from './meetPlayers';
 //import Promotion from './promotion';
+
+const Home = () => {
+
+
+};
+
+export default Home;
