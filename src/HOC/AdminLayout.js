ec373ee59c54c2e500ce9ad71809451545cdd49b
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2019-01-16 22:15:07 -0600

    Adding the home main component

--- src/HOC/AdminLayout.js
+++ src/HOC/AdminLayout.js
@@ -3,6 +3,17 @@ import React from 'react';
 
 const AdminLayout = (props) => {
 
+    return (
+        <div className="admin_container">
+            <div className="admin_left_nav">
+                <AdminNav/>
+            </div>
+            <div className="admin_right">
+                {props.children}
+            </div>
+        </div>
+    );
+
 
 };
 
