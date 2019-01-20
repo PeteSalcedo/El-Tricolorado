5a50154ff669730a3695b7744ec4c628b52ac624
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2019-01-19 21:56:09 -0600

    adding layout for the routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Layout from './Hoc/Layout';
 
-const Routes = (props) => {
+import { Switch, Route }  from 'react-router-dom';
 
+import PrivateRoute from './Components/authRoutes/privateRoutes';
+import PublicRoute from './Components/authRoutes/publicRoutes';
+
+
+const Routes = (props) => {
+    return(
+    <Layout/>
+        )
 
 }
 
